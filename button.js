c398afc33c2c3b7bebf320fb48b98bba040d5284
HinanoOno <107801598+HinanoOno@users.noreply.github.com>
2022-09-05 10:27:32 +0900

    コメント欄出てくる

---
+++ button.js
@@ -0,0 +1,42 @@
+const heartButton=document.querySelector('.heart_button');
+var numberSum=[];
+const storage=localStorage;
+heartButton.addEventListener('click',() => {
+  
+  var thisCount=$('.count_number').html();
+  thisCount=Number(thisCount);
+  thisCount=thisCount + 1;
+  $('.count_number').html(thisCount);
+
+  $('.heart_button,.heart_icon').addClass('clicked');
+
+
+  const number = {
+    currentNumber: thisCount
+  }
+  numberSum.push(number);
+  storage.store = JSON.stringify(numberSum); 
+  console.log(storage.store)
+
+},{'once':true})
+
+document.addEventListener("DOMContentLoaded", () => {
+  const json = storage.store;
+  numberSum = JSON.parse(json);
+  
+  
+  /**for(const item of numberSum ){
+    console.log(Number(item.currentNumber))
+  }**/
+  const lastNumber=(Number(numberSum[(numberSum.length)-1].currentNumber))
+  $('.count_number').html(lastNumber);
+  
+})
+/**delete storage.store;**/
+
+
+
+
+
+
+  
