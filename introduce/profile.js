202fe07c2de9dd0e89c93251e1ebbd8dfe486e44
kazuki1023 <k.keio12569@gmail.com>
2022-09-05 21:18:04 +0900

    現状

--- introduce/profile.js
+++ introduce/profile.js
@@ -146,120 +146,118 @@ submitButton.addEventListener('click',function(){
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   
-  const data=local.store;
+//   const data=local.store;
   
-  listContent=JSON.parse(data);
-  console.log(data)
-  console.log(listContent)
-  for (const item of listContent){
-    console.log(item.name)
-    const subnameContent=item.subname;
+//   listContent=JSON.parse(data);
+//   console.log(data)
+//   console.log(listContent)
+//   for (const item of listContent){
+//     console.log(item.name)
+//     const subnameContent=item.subname;
     
-    if(subnameContent[0]==='あ' || subnameContent[0]==='い' || subnameContent[0]==='う' ||subnameContent[0]==='え' ||subnameContent[0]==='お'){
-      const profileBox1=document.querySelector('.introduce_page1')
-      const litag=document.createElement('li');
-      const ptag=document.createElement('p');
+//     if(subnameContent[0]==='あ' || subnameContent[0]==='い' || subnameContent[0]==='う' ||subnameContent[0]==='え' ||subnameContent[0]==='お'){
+//       const profileBox1=document.querySelector('.introduce_page1')
+//       const litag=document.createElement('li');
+//       const ptag=document.createElement('p');
       
-      console.log(item.name);      
-      const facultyContent=item.faculty;
-      const universityContent=item.university;
-      const nameContent=item.name;
-      const gradesContent=item.grades;
-      const commentContent=item.comment;
-      nameInput.value='';
-      subnameInput.value='';
-      facultyInput.value='';
-      universityInput.value='';
-      gradesInput.value='';
-      commentInput.value='';
-      
-      
-      ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' + commentContent;
-      ptag.setAttribute('class','myinf_item')
-      litag.setAttribute('class','myinf_list')
-      litag.appendChild(ptag)
-      profileBox1.appendChild(litag)
-    }
-    else if(subnameContent[0]==='か' || subnameContent[0]==='き' || subnameContent[0]==='く' ||subnameContent[0]==='け' ||subnameContent[0]==='こ'){
-      const profileBox2=document.querySelector('.introduce_page2')
-      const litag=document.createElement('li');
-      const ptag=document.createElement('p');
-      
-      console.log(item.name);      
-      const facultyContent=item.faculty;
-      const universityContent=item.university;
-      const nameContent=item.name;
-      const gradesContent=item.grades;
-      const commentContent=item.comment;
-      nameInput.value='';
-      subnameInput.value='';
-      facultyInput.value='';
-      universityInput.value='';
-      gradesInput.value='';
-      commentInput.value='';
+//       console.log(item.name);      
+//       const facultyContent=item.faculty;
+//       const universityContent=item.university;
+//       const nameContent=item.name;
+//       const gradesContent=item.grades;
+//       const commentContent=item.comment;
+//       nameInput.value='';
+//       subnameInput.value='';
+//       facultyInput.value='';
+//       universityInput.value='';
+//       gradesInput.value='';
+//       commentInput.value='';
       
       
-      ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' +commentContent;
-      ptag.setAttribute('class','myinf_item')
-      litag.setAttribute('class','myinf_list')
-      litag.appendChild(ptag)
-      profileBox2.appendChild(litag)
-    }
-    else if(subnameContent[0]==='さ' || subnameContent[0]==='し' || subnameContent[0]==='す' ||subnameContent[0]==='せ' ||subnameContent[0]==='そ'){
-      const profileBox3=document.querySelector('.introduce_page3')
-      const litag=document.createElement('li');
-      const ptag=document.createElement('p');
-      const facultyContent=item.faculty;
-      const universityContent=item.university;
-      const nameContent=item.name;
-      const gradesContent=item.grades;
-      const commentContent=item.comment;
-      nameInput.value='';
-      subnameInput.value='';
-      facultyInput.value='';
-      universityInput.value='';
-      gradesInput.value='';
-      commentInput.value='';
+//       ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' + commentContent;
+//       ptag.setAttribute('class','myinf_item')
+//       litag.setAttribute('class','myinf_list')
+//       litag.appendChild(ptag)
+//       profileBox1.appendChild(litag)
+//     }
+//     else if(subnameContent[0]==='か' || subnameContent[0]==='き' || subnameContent[0]==='く' ||subnameContent[0]==='け' ||subnameContent[0]==='こ'){
+//       const profileBox2=document.querySelector('.introduce_page2')
+//       const litag=document.createElement('li');
+//       const ptag=document.createElement('p');
+      
+//       console.log(item.name);      
+//       const facultyContent=item.faculty;
+//       const universityContent=item.university;
+//       const nameContent=item.name;
+//       const gradesContent=item.grades;
+//       const commentContent=item.comment;
+//       nameInput.value='';
+//       subnameInput.value='';
+//       facultyInput.value='';
+//       universityInput.value='';
+//       gradesInput.value='';
+//       commentInput.value='';
       
       
-      ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' +commentContent;
-      ptag.setAttribute('class','myinf_item')
-      litag.setAttribute('class','myinf_list')
-      litag.appendChild(ptag)
-      profileBox3.appendChild(litag)
-    }
-    else if(subnameContent[0]==='た' || subnameContent[0]==='ち' || subnameContent[0]==='つ' ||subnameContent[0]==='て' ||subnameContent[0]==='と'){
-      const profileBox4=document.querySelector('.introduce_page4')
-      const litag=document.createElement('li');
-      const ptag=document.createElement('p');
-      
-      console.log(item.name);      
-      const facultyContent=item.faculty;
-      const universityContent=item.university;
-      const nameContent=item.name;
-      const gradesContent=item.grades;
-      const commentContent=item.comment;
-      nameInput.value='';
-      subnameInput.value='';
-      facultyInput.value='';
-      universityInput.value='';
-      gradesInput.value='';
-      commentInput.value='';
+//       ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' +commentContent;
+//       ptag.setAttribute('class','myinf_item')
+//       litag.setAttribute('class','myinf_list')
+//       litag.appendChild(ptag)
+//       profileBox2.appendChild(litag)
+//     }
+//     else if(subnameContent[0]==='さ' || subnameContent[0]==='し' || subnameContent[0]==='す' ||subnameContent[0]==='せ' ||subnameContent[0]==='そ'){
+//       const profileBox3=document.querySelector('.introduce_page3')
+//       const litag=document.createElement('li');
+//       const ptag=document.createElement('p');
+//       const facultyContent=item.faculty;
+//       const universityContent=item.university;
+//       const nameContent=item.name;
+//       const gradesContent=item.grades;
+//       const commentContent=item.comment;
+//       nameInput.value='';
+//       subnameInput.value='';
+//       facultyInput.value='';
+//       universityInput.value='';
+//       gradesInput.value='';
+//       commentInput.value='';
       
       
-      ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' +commentContent;
-      ptag.setAttribute('class','myinf_item')
-      litag.setAttribute('class','myinf_list')
-      litag.appendChild(ptag)
-      profileBox4.appendChild(litag)
-    }
+//       ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' +commentContent;
+//       ptag.setAttribute('class','myinf_item')
+//       litag.setAttribute('class','myinf_list')
+//       litag.appendChild(ptag)
+//       profileBox3.appendChild(litag)
+//     }
+//     else if(subnameContent[0]==='た' || subnameContent[0]==='ち' || subnameContent[0]==='つ' ||subnameContent[0]==='て' ||subnameContent[0]==='と'){
+//       const profileBox4=document.querySelector('.introduce_page4')
+//       const litag=document.createElement('li');
+//       const ptag=document.createElement('p');
+      
+//       console.log(item.name);      
+//       const facultyContent=item.faculty;
+//       const universityContent=item.university;
+//       const nameContent=item.name;
+//       const gradesContent=item.grades;
+//       const commentContent=item.comment;
+//       nameInput.value='';
+//       subnameInput.value='';
+//       facultyInput.value='';
+//       universityInput.value='';
+//       gradesInput.value='';
+//       commentInput.value='';     
+//       ptag.innerHTML = '大学名:'+ '<span></span>' +universityContent + '<br>' + '学年:'+ '<span></span>'+ gradesContent +'<br>' +'学部・学科:' + '<span></span>' +facultyContent + '<br>'+ 'ふりがな:'+ '<span></span>' +subnameContent +'<br>' + '名前:' +'<span></span>'  + nameContent +'<br>' +'コメント:' + '<span></span>' +commentContent;
+//       ptag.setAttribute('class','myinf_item')
+//       litag.setAttribute('class','myinf_list')
+//       litag.appendChild(ptag)
+//       profileBox4.appendChild(litag)
+//     }
   
     
     
-  }
-})
+//   }
+// })
 // localStorage.clear();  
 //↑これのコメントを解除すれば保存されてるデータが消える
 /*:delete local.store;**/
--- lengthOfThought/heart.js
+++ lengthOfThought/heart.js
@@ -1,90 +1,11 @@
 const button = document.getElementById('comment_btn');
 const replyBox = document.getElementById('comment_reply');
 const commentBox = document.getElementById('comment_box')
-
+const box = document.querySelector('container_content')
 button.addEventListener('click', () => {
     replyBox.classList.add('is-clicked');
     commentBox.classList.add('is-clicked');
 })
-
-
-// ここから送信ボタンを押したら、下に出てくるものを作っていきたい
-
-// まずここで入力するフォーム部分を取ってくる
-const ageInput=document.querySelector('.age_input');
-const monthsInput=document.querySelector('.months_input');
-const daysInput=document.querySelector('.days_input');
-// const subnameInput=document.querySelector('.heart_subname_input');
-const nameInput=document.querySelector('.heart_name_input');
-const commentInput = document.querySelector('.heart_comment_input');
-const submitButton=document.querySelector('.heart_submit_button');
-
-// ここで格納する
-var heartContents = [];
-const st = localStorage;
-
-// 投稿ボタンを押すと下の箱に内容が入る　ものを作っていく
-submitButton.addEventListener('click', function(){
-    var myheart = {
-        age: ageInput.value,
-        month: monthsInput.value,
-        day: daysInput.value,
-        // subname: subnameInput.value,
-        name: nameInput.value,
-        comment: commentInput.value,
-    }
-    heartContents.push(myheart);
-    // myheartに入ってるものをheartContents内に入れる
-    st.store=JSON.stringify(heartContents);
-
-    const heartdiv= document.createElement('div');
-    const heartbox= document.querySelector('.container_content_date');
-    const heartboxAge = document.querySelector('.container_content_date_age');
-    const heartboxMonth = document.querySelector('.container_content_date_month');
-    const heartboxDay = document.querySelector('.container_content_date_day');
-    const heartboxName = document.querySelector('.container_content_date_name');
-    const heartboxDetail = document.querySelector('.container_content_detail');
-    const heartboxBtn = document.querySelector('.container_content_btn');
-    const heartboxCommentBtn = document.querySelector('.container_content_comment_btn');
-    const heartboxCommentHeart = document.querySelector('.heart_button')
-    
-
-    const ageContent = ageInput.value;
-    const monthsContent = monthsInput.value;
-    const daysContent =  daysInput.value;
-    // const subnameContent = subnameInput.value;
-    const nameContent = nameInput.value;
-    const commentContent = commentInput.value;
-    
-    ageInput.value='';
-    monthsInput.value='';
-    daysInput.value='';
-    // subnameInput.value='';
-    nameInput.value='';
-    commentInput.value='';
-    // 入力した後にこれで空にする
-
-    // ここで入力した要素がどの要素に入るかを記述
-    heartboxAge.innerHTML = ageContent + '<span></span>';
-    heartboxMonth.innerHTML = monthsContent + '<span></span>';
-    heartboxDay.innerHTML = daysContent + '<span></span>';
-    heartboxName.innerHTML = '名前：' + nameContent ;
-    heartboxDetail.innerHTML = commentContent;
-    heartdiv.setAttribute('class', 'container_content')
-
-    // ここにどの要素がどの要素の子要素、親要素なのかを記述
-    heartdiv.appendChild(heartbox);
-    heartdiv.appendChild(heartboxDetail);
-    heartdiv.appendChild(heartboxBtn);
-    heartbox.appendChild(heartboxAge);
-    heartbox.appendChild(heartboxMonth);
-    heartbox.appendChild(heartboxDay);
-    heartbox.appendChild(heartboxName);
-    heartboxBtn.appendChild(heartboxCommentBtn);
-    heartboxBtn.appendChild(heartboxCommentHeart);
-
-})
-
 
 /**コメントボックス */
 
@@ -179,6 +100,93 @@ document.addEventListener("DOMContentLoaded", () => {
     
 
 })
+
+
+// ここから送信ボタンを押したら、下に出てくるものを作っていきたい
+
+// まずここで入力するフォーム部分を取ってくる
+const ageInput=document.querySelector('.age_input');
+const monthsInput=document.querySelector('.months_input');
+const daysInput=document.querySelector('.days_input');
+// const subnameInput=document.querySelector('.heart_subname_input');
+const nameInput=document.querySelector('.heart_name_input');
+const commentInput = document.querySelector('.heart_comment_input');
+const submitButton=document.querySelector('.heart_submit_button');
+
+// ここで格納する
+var heartContents = [];
+const st = localStorage;
+
+// 投稿ボタンを押すと下の箱に内容が入る　ものを作っていく
+submitButton.addEventListener('click', function(){
+    var myheart = {
+        age: ageInput.value,
+        month: monthsInput.value,
+        day: daysInput.value,
+        // subname: subnameInput.value,
+        name: nameInput.value,
+        comment: commentInput.value,
+    }
+    heartContents.push(myheart);
+    // myheartに入ってるものをheartContents内に入れる
+    st.store=JSON.stringify(heartContents);
+    
+    const heartContainer = document.querySelector('.container');
+    const heartdiv= document.createElement('div');
+    const heartbox= document.querySelector('.container_content_date');
+    const heartboxAge = document.querySelector('.container_content_date_age');
+    const heartboxMonth = document.querySelector('.container_content_date_month');
+    const heartboxDay = document.querySelector('.container_content_date_day');
+    const heartboxName = document.querySelector('.container_content_date_name');
+    const heartboxDetail = document.querySelector('.container_content_detail');
+    const heartboxBtn = document.querySelector('.container_content_btn');
+    const heartboxCommentBtn = document.querySelector('container_content_comment_btn');
+    const heartboxCommentHeart = document.querySelector('.heart_btn');
+    const heartboxPastComment = document.querySelector('past_comment');
+    
+    console.log(heartboxPastComment);
+
+    const ageContent = ageInput.value;
+    const monthsContent = monthsInput.value;
+    const daysContent =  daysInput.value;
+    // const subnameContent = subnameInput.value;
+    const nameContent = nameInput.value;
+    const commentContent = commentInput.value;
+    
+    ageInput.value='';
+    monthsInput.value='';
+    daysInput.value='';
+    // subnameInput.value='';
+    nameInput.value='';
+    commentInput.value='';
+    // 入力した後にこれで空にする
+
+    // ここで入力した要素がどの要素に入るかを記述
+    // heartboxAge.innerHTML = ageContent + '<span></span>';
+    // heartboxMonth.innerHTML = monthsContent + '<span></span>';
+    // heartboxDay.innerHTML = daysContent + '<span></span>';
+    // heartboxName.innerHTML = '名前：' + nameContent ;
+    // heartboxDetail.innerHTML = commentContent;
+    heartdiv.setAttribute('class', 'container_content_new')
+    heartboxDetail.innerHTML = ageContent + '<span></span>' + monthsContent + '<span></span>' + daysContent + '<span></span>' + '<br>' + '名前：' + '<span></span>' + nameContent + '<br>' + commentContent;
+
+    // ここにどの要素がどの要素の子要素、親要素なのかを記述
+    // heartdiv.appendChild(heartbox);
+    heartdiv.appendChild(heartboxDetail);
+    heartdiv.appendChild(heartboxBtn);
+    // heartdiv[0].appendChild(heartboxPastComment);
+    // heartdiv.appendChild(heartboxBtn);
+    // heartbox.appendChild(heartboxAge);
+    // heartbox.appendChild(heartboxMonth);
+    // heartbox.appendChild(heartboxDay);
+    // heartbox.appendChild(heartboxName);
+    // heartboxBtn.appendChild(heartboxCommentBtn);
+    // heartboxBtn.appendChild(heartboxCommentHeart);
+    heartContainer.insertBefore(heartdiv);
+})
+
+
+
 
 /*delete storage.store;*/
 /*delete keep.box*/
