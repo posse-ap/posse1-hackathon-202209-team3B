a280c3ec753fdafb54969d5f14d8fa6f5868ebc5
HinanoOno <107801598+HinanoOno@users.noreply.github.com>
2022-09-05 16:34:02 +0900

    統一

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ const ageInput=document.querySelector('.age_input');
 const monthsInput=document.querySelector('.months_input');
 const daysInput=document.querySelector('.days_input');
 const subnameInput=document.querySelector('.heart_subname_input');
-const nameInput=document.querySelectorAll('.heart_name_input');
+const nameInput=document.querySelector('.heart_name_input');
 const commentInput = document.querySelector('.heart_comment_input');
 const submitButton=document.querySelector('.heart_submit_button');
 
@@ -61,11 +61,111 @@ submitButton.addEventListener('click', function(){
     heartboxDay.innerHTML = daysContent + '日' + '<span></span>';
     heartboxName.innerHTML = '名前：' + nameContent ;
     heartboxContent.innerHTML = commentContent;
-    console.log(heartboxContent.innerHTML)
-    console.log(nameContent)
 
     heartboxContent.setAttribute('class', 'container_content_detail_ptag');
     // createElementで作った要素にclass名をつけた
 
     heartboxDetail.appendChild(heartboxContent);
 })
+
+
+/**コメントボックス */
+
+button.addEventListener('click', () => {
+    commentBox.style.display='block';
+    $('.container_content_comment_btn').addClass('clicked');
+    
+})
+
+
+/**ハートボタン */
+const heartButton=document.querySelector('.heart_button');
+var numberSum=[];
+const storage=localStorage;
+heartButton.addEventListener('click',() => {
+  
+  var thisCount=$('.count_number').html();
+  thisCount=Number(thisCount);
+  thisCount=thisCount + 1;
+  $('.count_number').html(thisCount);
+
+  $('.heart_button,.heart_icon').addClass('clicked');
+
+
+  const number = {
+    currentNumber: thisCount
+  }
+  numberSum.push(number);
+  storage.store = JSON.stringify(numberSum); 
+  console.log(storage.store)
+
+},{'once':true})
+
+document.addEventListener("DOMContentLoaded", () => {
+  const json = storage.store;
+  numberSum = JSON.parse(json);
+  
+  
+  /**for(const item of numberSum ){
+    console.log(Number(item.currentNumber))
+  }**/
+  const lastNumber=(Number(numberSum[(numberSum.length)-1].currentNumber))
+  $('.count_number').html(lastNumber);
+  
+})
+
+/**過去のコメント表示 */
+const commentInput2=document.querySelector('.comment_input');
+const replyBtn=document.querySelector('.reply_button');
+
+var heartContents=[ ];
+const keep=localStorage;
+
+replyBtn.addEventListener('click',function(){
+  
+    var pastComment={
+      comment: commentInput2.value,
+    }
+    heartContents.push(pastComment);
+    keep.box=JSON.stringify(heartContents);
+    const  commentResult=document.querySelector('.past_comment');
+
+    const commentContent=commentInput2.value;
+    console.log(commentContent)
+    const ptag=document.createElement('p');
+    ptag.setAttribute('class','comment_ptag')
+    commentInput.value='';
+
+    ptag.textContent= commentContent;
+    commentResult.appendChild(ptag);
+
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+    const keepBox=keep.box;
+    heartContents=JSON.parse(keepBox);
+
+    button.addEventListener('click',function (){
+        for (const item of heartContents){
+    
+            const replyContent =item.comment;
+            const ptag=document.createElement('p');
+            ptag.setAttribute('class','comment_ptag')
+            commentInput.value='';
+    
+            ptag.textContent= replyContent;
+            const  commentResult=document.querySelector('.past_comment');
+    
+            commentResult.appendChild(ptag);
+    
+        }
+
+    },{'once':true})
+    
+
+})
+
+/*delete storage.store;*/
+/*delete keep.box*/
+
